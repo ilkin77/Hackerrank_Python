23ef260f17f21c2e5f3a790ff66b6b0ce907c858
ilkin77 <ilkin78952@gmail.com>
2020-09-03 23:42:32 +0400

    String Validators

---
+++ String Validators.py
@@ -0,0 +1,50 @@
+if __name__ == '__main__':
+    s = input()
+
+
+def alnum(s):
+    for i in range(len(s)):
+        if (s[i].isalnum()):
+            return True
+            break
+    return False
+
+
+def alpha(s):
+    for i in range(len(s)):
+        if (s[i].isalpha()):
+            return True
+            break
+    return False
+
+
+def digit(s):
+    for i in range(len(s)):
+        if (s[i].isdigit()):
+            return True
+            break
+    return False
+
+
+def lower(s):
+    for i in range(len(s)):
+        if (s[i].islower()):
+            return True
+            break
+    return False
+
+
+def upper(s):
+    for i in range(len(s)):
+        if (s[i].isupper()):
+            return True
+            break
+    return False
+
+
+print(alnum(s))
+print(alpha(s))
+print(digit(s))
+print(lower(s))
+print(upper(s))
+
